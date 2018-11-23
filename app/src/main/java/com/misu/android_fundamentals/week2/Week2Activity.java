0ef9f619006c6a7220a50b5278a6fffa9a13db40
Mihai <mihai.constantin98@gmail.com>
2018-11-23 22:04:10 +0200

    Android

---
+++ app/src/main/java/com/misu/android_fundamentals/week2/Week2Activity.java
@@ -0,0 +1,15 @@
+package com.misu.android_fundamentals.week2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.misu.android_fundamentals.R;
+
+public class Week2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week2);
+    }
+}
