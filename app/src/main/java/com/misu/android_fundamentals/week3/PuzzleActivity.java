6dad531af16da237f4b1e9e9af2640a0170acf44
Mihai <mihai.constantin98@gmail.com>
2018-11-24 14:19:01 +0200

    Android

--- app/src/main/java/com/misu/android_fundamentals/week3/PuzzleActivity.java
+++ app/src/main/java/com/misu/android_fundamentals/week3/PuzzleActivity.java
@@ -10,6 +10,6 @@ public class PuzzleActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_puzzle);
+        setContentView(R.layout.activity_puzzle2);
     }
 }
